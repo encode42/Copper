d7262f98d39a51f1a77682d6907d37b04aa488b7
Encode42 <me@encode42.dev>
2021-01-10 20:07:02 -0500

    Update for MatrixChecks

---
+++ src/main/java/dev/encode42/encodedapi/Command.java
@@ -0,0 +1,55 @@
+package dev.encode42.encodedapi;
+
+import cloud.commandframework.CommandTree;
+import cloud.commandframework.annotations.AnnotationParser;
+import cloud.commandframework.arguments.parser.StandardParameters;
+import cloud.commandframework.bukkit.BukkitCommandManager;
+import cloud.commandframework.bukkit.BukkitCommandMetaBuilder;
+import cloud.commandframework.bukkit.CloudBukkitCapabilities;
+import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator;
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.paper.PaperCommandManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+public class Command {
+	private static BukkitCommandManager<CommandSender> manager;
+	private static AnnotationParser<CommandSender> annotationParser;
+
+	/**
+	 * Create the command manager and parse commands
+	 * @param plugin The plugin instance
+	 * @param commands Commands to parse
+	 */
+	public static void run(Plugin plugin, Object ...commands) {
+		Logger log = plugin.getLogger();
+
+		// Coordinator and mapper
+		Function<CommandTree<CommandSender>, CommandExecutionCoordinator<CommandSender>> coordinator = AsynchronousCommandExecutionCoordinator.<CommandSender>newBuilder().build();
+		Function<CommandSender, CommandSender> mapper = Function.identity();
+
+		try {
+			manager = new PaperCommandManager<>(plugin, coordinator, mapper, mapper);
+		} catch (Exception e) {
+			log.severe("An error occurred while initializing the command framework. " + e);
+			plugin.getServer().getPluginManager().disablePlugin(plugin);
+		}
+
+		// Version and software specific features
+		if (manager.queryCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) ((PaperCommandManager<CommandSender>) manager).registerAsynchronousCompletions();
+		if (manager.queryCapability(CloudBukkitCapabilities.BRIGADIER)) manager.registerBrigadier();
+
+		// Annotation parser
+		annotationParser = new AnnotationParser<>(manager,
+				CommandSender.class,
+				p -> BukkitCommandMetaBuilder.builder()
+						.withDescription(p.get(StandardParameters.DESCRIPTION, "No description provided."))
+						.build());
+
+		// Parse the commands
+		for (Object c : commands) annotationParser.parse(c);
+	}
+}
--- src/main/java/dev/encode42/encodedapi/Config.java
+++ src/main/java/dev/encode42/encodedapi/Config.java
@@ -4,71 +4,182 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
 
+/*
+TODO:
+- Seems like non-copy configurations may not be working
+- Long term: Defaults and migrations
+ */
 public class Config {
-    private YamlConfiguration config;
-    private final String filename;
-    private final Plugin plugin;
-    private final Logger log;
+	private FileConfiguration config;
+	private final boolean isCopy;
 
-    public Config(Plugin plugin, String filename) {
-        this.filename = filename;
-        this.plugin = plugin;
-        this.log = plugin.getLogger();
-    }
+	private final Plugin plugin     = Util.plugin;
+	private final File pluginFolder = plugin.getDataFolder();
 
-    // Get the config
-    public FileConfiguration get() {
-        File configFile = getFile();
+	private final String filename;
+	private final File file;
 
-        // Create the file if not existent
-        if (!configFile.exists()) {create();}
+	/**
+	 * Configuration file manager
+	 * @param filename Name of the file
+	 * @param copy Make a copy
+	 * - Copies do not remove comments
+	 * - Copies disable all set/save methods
+	 */
+	public Config(String filename, boolean copy) {
+		this.filename = filename;
+		this.file     = Util.toFile(filename + ".yml");
+		this.isCopy   = copy;
 
-        return YamlConfiguration.loadConfiguration(configFile);
-    }
+		// Create and load the config
+		create();
+		load();
+	}
 
-    // Get the file
-    public File getFile() {
-        return new File(plugin.getDataFolder() + File.separator + filename.replaceAll("/\\\\", File.separator) + ".yml");
-    }
+	/**
+	 * Configuration file manager
+	 * Overload that defaults copy to false
+	 * @see Config(String, Boolean)
+	 * @param filename Name or path to save to
+	 */
+	public Config(String filename) {
+		this(filename, false);
+	}
 
-    // Create the file
-    public void create() {
-        try {
-            // Create the plugin folder
-            if (!plugin.getDataFolder().exists()) {plugin.getDataFolder().mkdir();}
+	/**
+	 * Create the configuration file
+	 */
+	private void create() {
+		try {
+			// Make the plugin directory
+			if (!pluginFolder.exists()) pluginFolder.mkdir();
 
-            // Create the config file
-            File configFile = getFile();
-            if (!configFile.exists()) {
-                configFile.createNewFile();
+			// Copy the config
+			if (this.isCopy) {
+				copy();
+				return;
+			}
 
-                Reader dataInput = new InputStreamReader(plugin.getResource(filename + ".yml"), StandardCharsets.UTF_8);
+			// Create the file
+			if (!file.exists()) file.createNewFile();
+			else return;
 
-                config = YamlConfiguration.loadConfiguration(dataInput);
-                save();
-            }
-        } catch(Exception e) {log.severe("Error creating data file: " + e);}
-    }
+			// Save the config
+			load();
+			save();
+		} catch(IOException e) {
+			Error.fileReadWriteError(file, e);
+		}
+	}
 
-    // Set config values
-    public void set(String key, Object value) {
-        File configFile = getFile();
-        config = YamlConfiguration.loadConfiguration(configFile);
+	/**
+	 * Create a the configuration as a copy
+	 * Disables save/set methods
+	 */
+	private void copy() {
+		try {
+			// Get the resource
+			Reader resource = Util.getResource(filename);
 
-        config.set(key, value);
-        save();
-    }
+			// Convert the reader to a string
+			BufferedReader reader = new BufferedReader(resource);
+			StringBuilder builder = new StringBuilder();
+			String line;
 
-    // Save the config
-    public void save() {
-        try {
-            File configFile = getFile();
-            config.save(configFile);
-        }
-        catch (Exception e) {log.severe("Error saving data file: " + e);}
-    }
+			while ((line = reader.readLine()) != null)
+				builder.append(line)
+						.append("\n");
+
+			// Write the file
+			Util.writeFile(file.getName(), builder.toString(), false);
+
+			reader.close();
+			resource.close();
+		} catch (IOException e) {
+			Error.fileReadWriteError(file, e);
+		}
+	}
+
+	/**
+	 * Save the configuration
+	 * Disabled if the configuration is a copy
+	 */
+	public void save() {
+		try {
+			if (!this.isCopy) config.save(file);
+		} catch (IOException e) {
+			Error.fileReadWriteError(file, e);
+		}
+	}
+
+	/**
+	 * Load the configuration from a reader
+	 * @param reader The reader to load
+	 */
+	private void load(Reader reader) {
+		config = YamlConfiguration.loadConfiguration(reader);
+
+		// Close the reader
+		try {
+			reader.close();
+		} catch (IOException e) {
+			Error.readerCloseError(e);
+		}
+	}
+
+	/**
+	 * Load the configuration from a file
+	 * @param file The file to load
+	 */
+	private void load(File file) {
+		config = YamlConfiguration.loadConfiguration(file);
+	}
+
+	/**
+	 * Load the configuration from a file
+	 * Overload that defaults to global file
+	 * @see Config#load(File)
+	 */
+	private void load() {
+		load(file);
+	}
+
+	/**
+	 * Get the config
+	 * @return The configuration file
+	 */
+	public FileConfiguration get() {
+		return config;
+	}
+
+	/**
+	 * Set configuration values
+	 * Disabled if the configuration is a copy
+	 * @param key Key (path) to set
+	 * @param value Value to set the key to
+	 */
+	public void set(String key, Object value) {
+		// Don't modify copies
+		if (this.isCopy) return;
+
+		config.set(key, value);
+		save();
+	}
+
+	/**
+	 * Reload the config
+	 * @return The configuration file
+	 */
+	public FileConfiguration reload() {
+		// Create and load
+		create();
+		load();
+
+		return get();
+	}
 }
