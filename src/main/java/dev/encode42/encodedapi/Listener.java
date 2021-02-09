daab3f00d787d64ad91cde5ada7812114c3a6c6c
Encode42 <me@encode42.dev>
2021-02-09 09:11:07 -0500

    No more recursive

--- src/main/java/dev/encode42/encodedapi/Listener.java
+++ src/main/java/dev/encode42/encodedapi/Listener.java
@@ -1,30 +1,14 @@
 package dev.encode42.encodedapi;
 
 import org.bukkit.Bukkit;
-import org.reflections.Reflections;
-
-import java.lang.annotation.Annotation;
 
 public class Listener {
 	/**
 	 * Register event listeners
 	 * @param listeners Listeners to register
 	 */
-	public static void register(Object ...listeners) {
+	public static void register(org.bukkit.event.Listener ...listeners) {
 		// Register the listeners
-		for (Object listener : listeners) Bukkit.getServer().getPluginManager().registerEvents(org.bukkit.event.Listener.class.cast(listener), Util.plugin);
-	}
-
-	/**
-	 * Register event listeners recursively in a package
-	 * @param path Path of listeners to register
-	 * @param registrationClass Class interface to check for
-	 * - Class must be in the same package as the path
-	 */
-	public static void recursiveRegister(String path, Class<? extends Annotation> registrationClass) {
-		Reflections reflections = new Reflections(new String[]{path});
-		for (Class<?> listener : reflections.getTypesAnnotatedWith(registrationClass)) {
-			Bukkit.getServer().getPluginManager().registerEvents(org.bukkit.event.Listener.class.cast(listener), Util.plugin);
-		};
+		for (org.bukkit.event.Listener listener : listeners) Bukkit.getServer().getPluginManager().registerEvents(listener, Util.plugin);
 	}
 }
