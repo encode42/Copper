a5c46974fc207cee472c167d0faa551b9f0fed00
Encode42 <me@encode42.dev>
2021-08-03 23:52:41 -0400

    Abstract listener helpers

--- bot/src/main/java/dev/encode42/copper/helper/JDAListenerHelper.java
+++ bot/src/main/java/dev/encode42/copper/helper/JDAListenerHelper.java
@@ -1,11 +1,33 @@
 package dev.encode42.copper.helper;
 
+import dev.encode42.copper.logger.OmniLogger;
 import net.dv8tion.jda.api.JDA;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 
 public class JDAListenerHelper extends ListenerHelper {
+    private JDA jda;
+
+    public JDAListenerHelper(String listenerPackage, Class<? extends Annotation> listenerClass, JDA jda, boolean register) {
+        super(listenerPackage, listenerClass, false);
+        this.jda = jda;
+
+        if (register) {
+            this.register();
+        }
+    }
+
+    public JDAListenerHelper(String listenerPackage, Class<? extends Annotation> listenerClass, JDA jda) {
+        this(listenerPackage, listenerClass, jda, false);
+    }
+
+    @Override
+    protected void run(Class<?> clazz) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        jda.addEventListener(clazz.getConstructor().newInstance());
+    }
+
+    /*
     private static JDA jda;
 
     public static void register(String listenerPackage, Class<? extends Annotation> listenerClass, JDA jda) {
@@ -13,7 +35,9 @@ public class JDAListenerHelper extends ListenerHelper {
         JDAListenerHelper.jda = jda;
     }
 
-    private void run(Class<?> clazz) throws InstantiationException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    protected static void run(Class<?> clazz) throws InstantiationException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        System.out.println(clazz.getSimpleName());
         jda.addEventListener(clazz.getConstructor().newInstance());
     }
+    */
 }
--- core/src/main/java/dev/encode42/copper/helper/ListenerHelper.java
+++ core/src/main/java/dev/encode42/copper/helper/ListenerHelper.java
@@ -1,17 +1,48 @@
 package dev.encode42.copper.helper;
 
+import dev.encode42.copper.logger.OmniLogger;
 import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
-public class ListenerHelper {
+public abstract class ListenerHelper extends AbstractListenerHelper {
+    public ListenerHelper(String listenerPackage, Class<? extends Annotation> listenerClass, boolean register) {
+        super(listenerPackage, listenerClass, register);
+    }
+
+    public ListenerHelper(String listenerPackage, Class<? extends Annotation> listenerClass) {
+        this(listenerPackage, listenerClass, false);
+    }
+
+    protected void run(Class<?> clazz) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        clazz.getConstructor().newInstance();
+    }
+}
+
+abstract class AbstractListenerHelper {
+    String listenerPackage;
+    Class<? extends Annotation> listenerClass;
+    Set<Class<?>> listeners = new HashSet<>();
+
     /**
      * Registers all listeners annotated with the Listener interface.
      * @author YouHaveTrouble
      */
-    public static void register(String listenerPackage, Class<? extends Annotation> listenerClass) {
+    public AbstractListenerHelper(String listenerPackage, Class<? extends Annotation> listenerClass, boolean register) {
+        this.listenerPackage = listenerPackage;
+        this.listenerClass = listenerClass;
+
+        if (register) {
+            this.register();
+        }
+    }
+
+    public void register() {
+        OmniLogger.info("Registering listeners...");
+
         // Find all listener packages
         Reflections reflections = new Reflections((Object) new String[]{listenerPackage});
         Set<Class<?>> listeners = reflections.getTypesAnnotatedWith(listenerClass);
@@ -19,19 +50,19 @@ public class ListenerHelper {
         // Iterate through the package
         for (Class<?> listener : listeners) {
             try {
-                run(listener);
+                this.run(listener);
+                listeners.add(listener);
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 e.printStackTrace();
             }
-
         }
+
+        OmniLogger.info(String.format("Listener registration done! Registered a total of %s listeners!", listeners.size()));
     }
 
-    /**
-     * Run the registration method
-     * @param clazz Class to be registered
-     */
-    private static void run(Class<?> clazz) throws InstantiationException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        clazz.getConstructor().newInstance();
+    protected abstract void run(Class<?> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
+
+    public Set<Class<?>> getListeners() {
+        return this.listeners;
     }
 }
