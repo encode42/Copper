bf0f57ae645c91d4e4a7cd8cf1e4ee1a8a2292bb
Encode42 <me@encode42.dev>
2021-09-06 23:05:17 -0400

    Modify constants slightly

--- core/src/main/java/dev/encode42/copper/util/Constants.java
+++ core/src/main/java/dev/encode42/copper/util/Constants.java
@@ -2,12 +2,14 @@ package dev.encode42.copper.util;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Constants {
 	/**
 	 * An empty InputStream.
 	 */
-	public static final InputStream emptyStream = new InputStream() {
+	public static final InputStream EMPTY_STREAM = new InputStream() {
 		@Override
 		public int read() {
 			return -1;
@@ -17,5 +19,10 @@ public class Constants {
 	/**
 	 * An empty File.
 	 */
-	public static final File emptyFile = new File("");
+	public static final File EMPTY_FILE = new File("");
+
+	/**
+	 * An empty string list.
+	 */
+	public static final List<String> EMPTY_STRING_LIST = new ArrayList<>();
 }
