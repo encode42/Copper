11c08e7dc28b94e2b93360299133277bcb319c4f
Encode42 <me@encode42.dev>
2021-12-22 22:36:51 -0500

    List to set

--- copper-core/src/main/java/dev/encode42/copper/util/SetUtil.java
+++ copper-core/src/main/java/dev/encode42/copper/util/SetUtil.java
@@ -1,15 +1,28 @@
 package dev.encode42.copper.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class SetUtil {
+    /**
+     * Convert a list into a set.
+     * @param list List to convert
+     * @return Converted set
+     */
+    public static Set<?> fromList(List<?> list) {
+        return new HashSet<>(list);
+    }
+
     /**
      * Parse a raw set to a string set.
      * @param set Set to parse and convert
      * @return Parsed set
      */
-    public static Set<String> toStandard(Set set) {
+    public static Set<String> toStandard(Set<?> set) {
         Set<String> parsed = new HashSet<>();
 
         for (Object object : set) {
@@ -25,7 +38,7 @@ public class SetUtil {
      * @param comparison Set to compare with
      * @return Whether the set contains at least one similar element
      */
-    public static boolean containsSome(Set set, Set comparison) {
+    public static boolean containsSome(Set<?> set, Set<?> comparison) {
         boolean contains = false;
 
         for (Object object : comparison) {
